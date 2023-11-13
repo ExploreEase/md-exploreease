27468c648cbd0b9e76c63ff3aff05c5c0bc686f0
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2023-11-13 14:44:01 +0700

    Onboarding UI

--- app/src/main/java/com/ch2ps418/travelapp/presentation/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/ch2ps418/travelapp/presentation/ui/onboarding/OnboardingActivity.kt
@@ -1,12 +1,43 @@
 package com.ch2ps418.travelapp.presentation.ui.onboarding
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.ch2ps418.travelapp.R
+import com.ch2ps418.travelapp.databinding.ActivityOnboardingBinding
+import com.ch2ps418.travelapp.presentation.ui.onboarding.carouselview.CarouselPage
+import com.ch2ps418.travelapp.presentation.ui.onboarding.carouselview.CarouselPager
+import com.ch2ps418.travelapp.presentation.ui.onboarding.carouselview.ZoomOutPageTransformer
 
-class OnboardingActivity : AppCompatActivity() {
+class OnboardingActivity : AppCompatActivity(), CarouselPager.CarouselListener {
+
+	private lateinit var binding: ActivityOnboardingBinding
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_onboarding)
+		binding = ActivityOnboardingBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+
+		val titles = resources.getStringArray(R.array.carousel_titles)
+		val subtitles = resources.getStringArray(R.array.carousel_subtitles)
+		val drawableIds = resources.obtainTypedArray(R.array.carousel_drawables)
+
+		val carouselPages = mutableListOf<CarouselPage>()
+		for (i in titles.indices) {
+			val drawableId = drawableIds.getResourceId(i, 0)
+			carouselPages.add(CarouselPage(drawableId, titles[i], subtitles[i]))
+		}
+
+		drawableIds.recycle()
+
+		binding.carouselPager
+			.setUpCarousel(this, carouselPages)
+			.setUpPageTransformer(ZoomOutPageTransformer())
+			.setUpCarouselListener(this)
+	}
+
+	override fun onCarouselFinished(skipped: Boolean) {
+		val carouselStatus = if (skipped) "skipped" else "completed"
+		Toast.makeText(this, "You've $carouselStatus the onboarding.", Toast.LENGTH_SHORT).show()
 	}
 }
---
+++ app/src/main/java/com/ch2ps418/travelapp/presentation/ui/onboarding/carouselview/CarouselPage.kt
@@ -0,0 +1,9 @@
+package com.ch2ps418.travelapp.presentation.ui.onboarding.carouselview
+
+import androidx.annotation.DrawableRes
+
+class CarouselPage(
+    @DrawableRes val image: Int,
+    val title: String,
+    val text: String
+)
