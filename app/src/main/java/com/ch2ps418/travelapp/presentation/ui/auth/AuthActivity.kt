dadb8df566d1c35d4c76f35a7e5c697e12e4730d
Muhammad Imaduddin Mustofa <l200200165@student.ums.ac.id>
2023-11-14 17:39:19 +0700

    Authentication Fragments

---
+++ app/src/main/java/com/ch2ps418/travelapp/presentation/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.ch2ps418.travelapp.presentation.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ch2ps418.travelapp.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
