49439961540cf4939235ed021028c71ecbd6ff23
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2023-11-30 00:23:44 +0700

    fixing permission

--- app/src/main/java/com/ch2ps418/travelapp/presentation/ui/home/home/HomeFragment.kt
+++ app/src/main/java/com/ch2ps418/travelapp/presentation/ui/home/home/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.ch2ps418.travelapp.presentation.ui.home.home
 
 import android.Manifest
-import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -83,6 +82,23 @@ class HomeFragment : Fragment() {
 	}
 
 	private fun getCurrentLocation() {
+		if (ActivityCompat.checkSelfPermission(
+				requireContext(),
+				Manifest.permission.ACCESS_FINE_LOCATION
+			) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+				requireContext(),
+				Manifest.permission.ACCESS_COARSE_LOCATION
+			) != PackageManager.PERMISSION_GRANTED
+		) {
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			return
+		}
 		fusedLocationClient.lastLocation.addOnSuccessListener { location ->
 			if (location != null) {
 				val latitude = location.latitude
